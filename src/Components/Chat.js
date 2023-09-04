67d2d571b5841f73e6e2eeae13bfd47f0ca17526
Andrew Stringfellow <abreezemail@gmail.com>
2023-09-04 17:43:07 -0400

    began chat view top bar...added background image for chat

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,10 +1,28 @@
 import { useState } from 'react'
 import "../css/Chat.css"
+import { Avatar } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import AttachFileIcon from '@mui/icons-material/AttachFile'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 const Chat = (props) => {
     return (
         <div className="Chat">
-            <div className="Chat-toolbar"></div>
+            <div className="Chat-toolbar">
+                <div className="Chat-toolbar-chatinfo">
+                    <Avatar></Avatar>
+                    <div className="Chat-toolbar-chatinfo-nametime">
+                        <h2>Chat One</h2>
+                        <h3>Last seen Fri, 03 2023 18:03:06 EST</h3>
+                    </div>
+                </div>
+                <div className="Chat-toolbar-buttons">
+                    <SearchIcon></SearchIcon>
+                    <AttachFileIcon></AttachFileIcon>
+                    <MoreVertIcon></MoreVertIcon>
+
+                </div>
+            </div>
             <div className="Chat-view"></div>
             <div className="Chat-message"></div>
         </div>
