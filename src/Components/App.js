fb830258f943587e6541c303e3b7f371ae655c0c
Andrew Stringfellow <abreezemail@gmail.com>
2023-09-05 23:10:03 -0400

    added mongo realtime...TODO: fix weird connection issue with request failed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,31 @@
 import '../css/App.css';
+import { useState, useEffect } from "react"
+
 import Conversations from './Conversations';
 import Chat from "./Chat";
+import * as Realm from "realm-web";
+
+const realmApp = new Realm.App({ id: "whatsapp-clone-nrzrz"})
 
 function App() {
+
+  const [ mongoRealtime, setMongoRealtime ] = useState()
+
+  useEffect(() => {
+    const login = async () => {
+      const realmUser = await realmApp.logIn(Realm.Credentials.anonymous())
+      const mongodb = realmApp.currentUser.mongoClient("mongodb-atlas")
+      setMongoRealtime(mongodb)
+    }
+
+    login()
+  }, [])
+
   return (
-    <div className="App-container">
-      <Conversations></Conversations>
-      <Chat></Chat>
-    </div>
+      <div className="App-container">
+        {mongoRealtime && <Conversations mongo={mongoRealtime}></Conversations>}
+        {mongoRealtime && <Chat mongo={mongoRealtime}></Chat>}
+      </div>
   );
 }
 
--- src/Components/Conversations.js
+++ src/Components/Conversations.js
@@ -1,11 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "../css/Conversations.css"
 import ChatRoom from './ChatRoom'
+
+// Material UI
 import { Avatar } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 import MessageIcon from '@mui/icons-material/Message'
 
+// Axios
+import axios from 'axios'
+axios.defaults.baseURL = "http://localhost:3005"
+
+
+
+
 const Conversations = (props) => {
+
+    const [ chatRooms, setChatRooms] = useState([])
+    const [ mongoConnection, setMongoConnection ] = useState()
+
+    useEffect(() => {
+        const chatRoomUseEffect = async () => {
+            const chatRooms = await axios.get("/chatroom/getChatRooms")
+            setChatRooms([...chatRooms.data])
+        }
+        chatRoomUseEffect()
+
+        const mongoListen = (async () => {
+            const chatRoomCollection = props.mongo.db("whatsapp-clone").collection("chatrooms")
+            console.log(chatRoomCollection)
+            console.log(await chatRoomCollection.find())
+
+            // for await (const change of chatRoomCollection.watch()) {
+            //     // setChatRooms([...chatRooms, change])
+            //     console.log(change)
+            // }
+        })
+        mongoListen()
+    }, [])
+
     return (
         <nav class="Conversations">
             <div className="Conversations-toolbar">
@@ -23,16 +56,7 @@ const Conversations = (props) => {
             </div>
             <div className="Conversations-list">
                 <h2 className="Conversations-list-add">Add New Chat</h2>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
-                <ChatRoom></ChatRoom>
+                {chatRooms.map((room) => <ChatRoom></ChatRoom>)}
             </div>
 
         </nav>
